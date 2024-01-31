9c94055e3cb88c9308c4c97c9dabba5de48cad52
Anders Gustav Jensen <80692647+AndersGustav@users.noreply.github.com>
2024-01-31 12:52:22 +0100

    Update lnws-kiosk-installer.sh

--- klokkekiosk/lnws-kiosk-installer.sh
+++ klokkekiosk/lnws-kiosk-installer.sh
@@ -71,8 +71,7 @@ do
     --disable-save-password-bubble \
     --disable-session-crashed-bubble \
     --incognito \
-    --start-fullscreen \
-    --lnws "http://scada.melbu.leroy.no/data/perspective/client/Infoskjermer/Video/Gyro3_Combined"
+    --lnws "http://scada.melbu.leroy.no/data/perspective/client/Klokke"
   sleep 5
 done &
 EOF
